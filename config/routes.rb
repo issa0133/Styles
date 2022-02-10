edf5ff0754a7ee4ccaad0bae4c94c11a8a5d70c6
issa0133 <94674045+issa0133@users.noreply.github.com>
2022-02-10 07:03:24 +0000

    [First]environment

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,24 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to = 'homes#top'
+  get 'about' => 'homes#about'
+  get 'search' => 'searches#search'
+  
+  resources :post_iamges, only: [:index, :new, :create, :show, :edit] do
+    collection do
+      get :follower_post
+    end
+    resources :post_comments, only: [:create, :destroy]
+    resource :post_favorites, only: [:create, :destroy]
+  end
+  
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get :followers, on: :member
+    get :followeds, on: :member
+  end
+  
+  resources :genres, only: [:index, :show]
   
 end
 
